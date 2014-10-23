d407f3542c22ec9c177a0232be8aa36effa9f998
purplecabbage <purplecabbage@gmail.com>
2014-10-23 02:30:05 -0700

    moved to own repo

--- pgd14Plugins/code/rapture/src/ios/Rapture.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <Cordova/CDVPlugin.h>
-
-@interface Rapture : CDVPlugin
-{}
-
-- (void)captureScreen:(CDVInvokedUrlCommand*)command;
-
-@end
--- pgd14Plugins/code/rapture/www/rapture.js
+++
@@ -1,5 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.captureScreen = function(success, error) {
-    exec(success, error, "rapture", "captureScreen", []);
-};
